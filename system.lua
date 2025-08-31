85bcc95bb98f64b5b7a6f0dc53379aa3644a7017
devzgroupscripts <kadoeditz@gmail.com>
2025-08-31 13:05:16 +0100

    system.lua

--- system.lua
+++ system.lua
@@ -1,23 +1,23 @@
 local validHWIDs = {
-    [9356628060] = true,
-    [9365772659] = true
+    [9356628060] = true
 }
 
 local player = game.Players.LocalPlayer
 local hwid = player.UserId
 
-if validHWIDs[hwid] then
-    print("✅ HWID eşleşti, script çalışacak...")
-    
-    -- Scripti yükle ve çalıştır
-    local success, err = pcall(function()
-        local code = game:HttpGet("https://raw.githubusercontent.com/devzgroupscripts/Bypass/refs/heads/main/system.lua", true)
-        loadstring(code)()
-    end)
-    
-    if not success then
-        warn("Script yüklenemedi: "..err)
-    end
-else
+if not validHWIDs[hwid] then
     print("❌ HWID eşleşmedi.")
+    return
+end
+
+print("✅ HWID eşleşti, script çalışıyor...")
+
+-- Scripti güvenli şekilde çalıştır
+local success, err = pcall(function()
+    local code = game:HttpGet("https://raw.githubusercontent.com/devzgroupscripts/Bypass/refs/heads/main/system.lua", true)
+    loadstring(code)()
+end)
+
+if not success then
+    warn("❌ Script yüklenemedi: "..err)
 end
